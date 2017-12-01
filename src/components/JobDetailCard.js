1b11a82dc9bc3ce6ff8e565ae49f39a96e415721
Michel Marrache <mmarrache@magicleap.com>
2017-11-30 21:14:20 -0500

    add google map to job detail page

--- src/components/JobDetailCard.js
+++ src/components/JobDetailCard.js
@@ -6,19 +6,13 @@ import styled from 'styled-components';
 
 import pickUpIcon from '../images/icons/pick.svg';
 import dropOffIcon from '../images/icons/drop.svg';
+import JobDetailMap from './JobDetailMap';
 
 const DropCitySpan = styled.span`
     font-weight: bold;
 `;
 
-// const distanceBetweenGeopoints = (lat1, lon1, lat2, lon2) => {
-//     console(lat1, lon1, lat2, lon2);
-//     var g = new GeoPoint(lat1, lon1);
-
-//     console.log(g, 'geopoint');
-// }
-
-const JobDetailCard = ({ job }) => (
+const JobDetailCard = ({ job, markerLocation, setMarkerLocation }) => (
     <div>
         {/* <div className="container status">
             <div className="row">
@@ -45,33 +39,39 @@ const JobDetailCard = ({ job }) => (
                         </span>
                     </div>
                 </div>
+                <hr className="double" />
+                <div className="col-12">
+                    { job.ShipType.tags.map((tag) => {
+                        return <span key={tag} className="badge meta">{tag}</span>
+                    })}
+                </div>
+                <hr className="double" />
                 <div className="row">
-                    <hr className="double" />
-                    <div className="col-12">
-                        { job.ShipType.tags.map((tag) => {
-                            return <span key={tag} className="badge meta">{tag}</span>
-                        })}
-                    </div>
-                    <hr className="double" />
-                    <div className="col-12 pickDrop">
+                    <div className="col-6">
                         <img src={pickUpIcon} alt="pickup" className="float-left pickIcon" />
                         <div className="float-left">
                             <label className="addressLabel">Pick Up</label>
                             <span className="address">{job.PickCity}, {job.PickState} {job.PickZip}</span>
                         </div>
                     </div>
-                    <hr className="double" />
-                    <div className="col-12 pickDrop">
+                    <div className="col-6">
                         <img src={dropOffIcon} alt="dropoff" className="float-left pickIcon" />
                         <div className="float-left">
                             <label className="addressLabel">Drop-off</label>
                             <span className="address">{job.DropCity}, {job.DropState} {job.DropZip}</span>
                         </div>
                     </div>
-                    <div className="col-12 map">
-                        <img src="images/map.png" width="100%" alt="map" />
-                    </div>
                 </div>
+                {job && job.location &&
+                    <div>
+                        <hr className="double" />
+                        <br />
+                        <p className="city">Pick Up Location</p>
+                        <br />
+                        <JobDetailMap location={job.location} />
+                        <hr className="double" />
+                    </div>
+                }
             </div>
         </div>
     </div>
@@ -85,4 +85,4 @@ JobDetailCard.defaultProps = {
     job: null,
 };
 
-export default pure(JobDetailCard)
+export default pure(JobDetailCard);
---
+++ src/components/JobDetailMap.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { compose, pure, withProps } from 'recompose';
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+} from 'react-google-maps';
+
+/* global google */
+
+const hoc = compose(
+  withProps({
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyBBIPRh7wPALSS_SgmRICN6ZlzSlawC4ok&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+  }),
+  withScriptjs,
+  withGoogleMap,
+  pure,
+);
+
+export const JobDetailMap = ({location}) => (
+  <GoogleMap
+    defaultZoom={12}
+    defaultCenter={new google.maps.LatLng(location.lat, location.lng)}
+  >
+    <Marker
+      position={{ lat: location.lat, lng: location.lng }}
+    />
+  </GoogleMap>
+);
+
+export default hoc(JobDetailMap);
